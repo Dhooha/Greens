88e084778977bc9b0123ead2e336b3c22e207096
Melanie <Melanie@DESKTOP-8AV3HLA.mshome.net>
2019-04-14 18:07:00 -0400

    test

---
+++ web/resources/js/jsfcrud.js
@@ -0,0 +1,11 @@
+/*Automatically generated
+ */
+
+function handleSubmit(args, dialog) {
+    var jqDialog = jQuery('#' + dialog);
+    if (args.validationFailed) {
+        jqDialog.effect('shake', {times: 3}, 100);
+    } else {
+        PF(dialog).hide();
+    }
+}
