ed0be92dc63fd0ef9f5836efd34139bdee02f0a0
Joshua Detwiler <joshde@vt.edu>
2019-05-02 21:44:23 -0400

    Fix string format in Constants for UserPhoto's

--- src/java/edu/vt/globals/Constants.java
+++ src/java/edu/vt/globals/Constants.java
@@ -38,7 +38,7 @@ public final class Constants {
     
     macOS (Unix or Linux): Specify the absolute directory path as shown below.
      */
-    public static final String PHOTOS_ABSOLUTE_PATH = "dir=C:/Users/Melanie/DocRoot/Greens";
+    public static final String PHOTOS_ABSOLUTE_PATH = "C:/Users/izzmo_000/DocRoot/Greens/";
     // public static final String PHOTOS_ABSOLUTE_PATH = "/home/cloudsd/Survey/SurveyUserPhotoStorage/";
 
     /*
