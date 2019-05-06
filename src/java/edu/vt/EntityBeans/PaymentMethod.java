2b5f735f611bf8109f8da67396a5689ddcb7fd6b
Joshua Detwiler <joshde@vt.edu>
2019-05-06 02:52:31 -0400

    Add implementation of Payment Methods
    I updated the SQL file because I realized we had a few mistakes in
    how long certain fields needed to be. I also increased the number
    of possible credit card types to be supported.
    
    I implemented the bean, facade, controller, and validator. I then
    built the gui that lists the Payment Methods with an accordion
    panel. The forms will call validators as necessary. For the edit
    and delete buttons, I had to create action listeners that prepared
    a 'selected' object by passing the active tab's payment method
    statically when the button got created.
    
    Other minor changes:
    * I updated the bundle file.
    * I updated the profile page to link to My Payment Methods
    * It works now?? Hopefully.

--- src/java/edu/vt/EntityBeans/PaymentMethod.java
+++ src/java/edu/vt/EntityBeans/PaymentMethod.java
@@ -1,6 +1,6 @@
 /*
- * Created by Melanie Trammell on 2019.04.16  * 
- * Copyright © 2019 Melanie Trammell. All rights reserved. * 
+ * Created by Joshua Detwiler, Melanie Trammell on 2019.05.04  * 
+ * Copyright © 2019 Joshua Detwiler, Melanie Trammell. All rights reserved. * 
  */
 package edu.vt.EntityBeans;
 
@@ -20,80 +20,118 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author Melanie
- */
+// The @Entity annotation designates this class as a JPA Entity class 
+// representing the PaymentMethod table in the GreensDB database.
 @Entity
+
+// Name of the database table represented
 @Table(name = "PaymentMethod")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "PaymentMethod.findAll", query = "SELECT p FROM PaymentMethod p")
-    , @NamedQuery(name = "PaymentMethod.findById", query = "SELECT p FROM PaymentMethod p WHERE p.id = :id")
-    , @NamedQuery(name = "PaymentMethod.findByNameOnCreditCard", query = "SELECT p FROM PaymentMethod p WHERE p.nameOnCreditCard = :nameOnCreditCard")
-    , @NamedQuery(name = "PaymentMethod.findByCreditCardNumber", query = "SELECT p FROM PaymentMethod p WHERE p.creditCardNumber = :creditCardNumber")
-    , @NamedQuery(name = "PaymentMethod.findByCreditCardType", query = "SELECT p FROM PaymentMethod p WHERE p.creditCardType = :creditCardType")
-    , @NamedQuery(name = "PaymentMethod.findByCreditCardExpiration", query = "SELECT p FROM PaymentMethod p WHERE p.creditCardExpiration = :creditCardExpiration")
-    , @NamedQuery(name = "PaymentMethod.findByBillingAddress1", query = "SELECT p FROM PaymentMethod p WHERE p.billingAddress1 = :billingAddress1")
-    , @NamedQuery(name = "PaymentMethod.findByBillingAddress2", query = "SELECT p FROM PaymentMethod p WHERE p.billingAddress2 = :billingAddress2")
-    , @NamedQuery(name = "PaymentMethod.findByBillingCity", query = "SELECT p FROM PaymentMethod p WHERE p.billingCity = :billingCity")
-    , @NamedQuery(name = "PaymentMethod.findByBillingState", query = "SELECT p FROM PaymentMethod p WHERE p.billingState = :billingState")
-    , @NamedQuery(name = "PaymentMethod.findByBillingZipcode", query = "SELECT p FROM PaymentMethod p WHERE p.billingZipcode = :billingZipcode")})
+    @NamedQuery(name = "PaymentMethod.findAll", 
+                query = "SELECT p FROM PaymentMethod p")
+    , @NamedQuery(name = "PaymentMethod.findById", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.id = :id")
+    , @NamedQuery(name = "PaymentMethod.findByNameOnCreditCard", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.nameOnCreditCard = :nameOnCreditCard")
+    , @NamedQuery(name = "PaymentMethod.findByCreditCardNumber", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.creditCardNumber = :creditCardNumber")
+    , @NamedQuery(name = "PaymentMethod.findByCreditCardType", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.creditCardType = :creditCardType")
+    , @NamedQuery(name = "PaymentMethod.findByCreditCardExpiration", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.creditCardExpiration = :creditCardExpiration")
+    , @NamedQuery(name = "PaymentMethod.findByBillingAddress1", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.billingAddress1 = :billingAddress1")
+    , @NamedQuery(name = "PaymentMethod.findByBillingAddress2", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.billingAddress2 = :billingAddress2")
+    , @NamedQuery(name = "PaymentMethod.findByBillingCity", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.billingCity = :billingCity")
+    , @NamedQuery(name = "PaymentMethod.findByBillingState", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.billingState = :billingState")
+    , @NamedQuery(name = "PaymentMethod.findByBillingZipcode", 
+                query = "SELECT p FROM PaymentMethod p WHERE p.billingZipcode = :billingZipcode")
+    /* Added functionalities. */
+    , @NamedQuery(name = "PaymentMethod.findPaymentMethodsByUserPrimaryKey", 
+            query = "SELECT p FROM PaymentMethod p WHERE p.userId.id = :primaryKey")
+})
+
 public class PaymentMethod implements Serializable {
 
+    /*
+    ========================================================
+    Instance variables representing the attributes (columns)
+    of the PaymentMethod table in the GreensDB database.
+    ========================================================
+     */
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id")
     private Integer id;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 100)
     @Column(name = "name_on_credit_card")
     private String nameOnCreditCard;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 19)
     @Column(name = "credit_card_number")
     private String creditCardNumber;
+    
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 10)
+    @Size(min = 1, max = 15)
     @Column(name = "credit_card_type")
     private String creditCardType;
+    
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 5)
+    @Size(min = 1, max = 7)
     @Column(name = "credit_card_expiration")
     private String creditCardExpiration;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 128)
     @Column(name = "billing_address1")
     private String billingAddress1;
+    
     @Size(max = 128)
     @Column(name = "billing_address2")
     private String billingAddress2;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 64)
     @Column(name = "billing_city")
     private String billingCity;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 2)
     @Column(name = "billing_state")
     private String billingState;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 10)
     @Column(name = "billing_zipcode")
     private String billingZipcode;
+    
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     @ManyToOne(optional = false)
     private User userId;
 
+    /*
+    =========================================================================
+    Class constructors for instantiating a PaymentMethod entity object to
+    represent a row in the PaymentMethod table in the GreensDB database.
+    =========================================================================
+     */
     public PaymentMethod() {
     }
 
@@ -101,7 +139,9 @@ public class PaymentMethod implements Serializable {
         this.id = id;
     }
 
-    public PaymentMethod(Integer id, String nameOnCreditCard, String creditCardNumber, String creditCardType, String creditCardExpiration, String billingAddress1, String billingCity, String billingState, String billingZipcode) {
+    public PaymentMethod(Integer id, String nameOnCreditCard, String creditCardNumber, String creditCardType, 
+            String creditCardExpiration, String billingAddress1, String billingCity, String billingState, 
+            String billingZipcode) {
         this.id = id;
         this.nameOnCreditCard = nameOnCreditCard;
         this.creditCardNumber = creditCardNumber;
@@ -113,6 +153,12 @@ public class PaymentMethod implements Serializable {
         this.billingZipcode = billingZipcode;
     }
 
+    /*
+    ======================================================
+    Getter and Setter methods for the attributes (columns)
+    of the PaymentMethod table in the GreensDB database.
+    ======================================================
+     */
     public Integer getId() {
         return id;
     }
@@ -201,6 +247,14 @@ public class PaymentMethod implements Serializable {
         this.userId = userId;
     }
 
+    /*
+    ================
+    Instance Methods
+    ================
+     */
+    /**
+     * @return Generates and returns a hash code value for the object with id
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -208,6 +262,13 @@ public class PaymentMethod implements Serializable {
         return hash;
     }
 
+    /**
+     * Checks if the PaymentMethod object identified by 'object' is the same as 
+     * the PaymentMethod object identified by 'id'
+     *
+     * @param object The PaymentMethod object identified by 'object'
+     * @return True if the PaymentMethod 'object' and 'id' are the same; otherwise, return False
+     */
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
@@ -223,7 +284,8 @@ public class PaymentMethod implements Serializable {
 
     @Override
     public String toString() {
-        return "edu.vt.EntityBeans.PaymentMethod[ id=" + id + " ]";
+        // Convert the PaymentMethod object's database primary key (Integer) to String type and return it.
+        return id.toString();
     }
     
 }
--- src/java/edu/vt/FacadeBeans/PaymentMethodFacade.java
+++ src/java/edu/vt/FacadeBeans/PaymentMethodFacade.java
@@ -1,18 +1,15 @@
 /*
- * Created by Melanie Trammell on 2019.04.16  * 
- * Copyright © 2019 Melanie Trammell. All rights reserved. * 
+ * Created by Joshua Detwiler, Melanie Trammell on 2019.05.04  * 
+ * Copyright © 2019 Joshua Detwiler, Melanie Trammell. All rights reserved. * 
  */
 package edu.vt.FacadeBeans;
 
 import edu.vt.EntityBeans.PaymentMethod;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-/**
- *
- * @author Melanie
- */
 @Stateless
 public class PaymentMethodFacade extends AbstractFacade<PaymentMethod> {
 
@@ -27,5 +24,31 @@ public class PaymentMethodFacade extends AbstractFacade<PaymentMethod> {
     public PaymentMethodFacade() {
         super(PaymentMethod.class);
     }
+    
+    /*
+    ====================================================
+    The following method is added to the generated code.
+    ====================================================
+     */
+    /**
+     * @param primaryKey is the Primary Key of the User entity in a table row in the database.
+     * @return a list of payment methods associated with the User whose primary key is primaryKey
+     */
+    public List<PaymentMethod> findPaymentMethodsByUserPrimaryKey(Integer primaryKey) {
+
+        return (List<PaymentMethod>) em.createNamedQuery("PaymentMethod.findPaymentMethodsByUserPrimaryKey")
+                .setParameter("primaryKey", primaryKey)
+                .getResultList();
+    }
+
+    /* The following methods are inherited from the parent AbstractFacade class:
+    
+        create
+        edit
+        find
+        findAll
+        remove
+     */
+    
     
 }
